6ab3e832cf17e9b08b0fbdcee06683efbb8cdfb4
Daniel Marks <daniel.marks@codecentric.de>
2017-10-25 00:07:14 +0200

    Simplified setup. Now using antigen only.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,6 @@
 #!/bin/zsh
-# setup zsh
-[ -z /bin/zsh ] && chsh /bin/zsh
-if [ ! -d "$HOME/.oh-my-zsh" ] ; then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-else
-  env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
-fi
-source .oh-my-zsh/oh-my-zsh.sh
-
 # zsh plugins
 [ ! -d $HOME/.antigen ] && git clone https://github.com/zsh-users/antigen.git ~/.antigen
-source ~/.antigen/antigen.zsh
-antigen bundle zsh-users/zsh-autosuggestions
-# Maybe include Z ?
-#  https://github.com/rupa/z
-
 
 # clone repo
 if [ ! -d $HOME/.dotfiles ] ; then
@@ -31,7 +17,6 @@ for d in backup swap undo; do
   [ ! -z $d ] && mkdir -p $HOME/.vim/$d
 done
 
-# setup vim
+# setup vim plugins
 [ ! -f $HOME/.vim/autoload/plug.vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 vim -c ":PlugStatus|PlugUpgrade|PlugInstall|PlugUpdate|q|q"
